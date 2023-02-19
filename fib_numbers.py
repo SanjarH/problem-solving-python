a9667c0bbe19603c5db2518a56623cee9fa1e958
SanjarH <sanjarhoshimi@gmail.com>
2023-02-19 19:54:06 +0100

    refactored and added new solution

--- fib_numbers.py
+++ fib_numbers.py
@@ -7,8 +7,11 @@ the two preceding ones, starting from 0 and 1. That is,
 F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1. """
 
+# importing lru_cache function for memoization
+from functools import lru_cache 
 # Solution
-def fibonacci(n):    
+@lru_cache(maxsize=None) 
+def fibonacci(n: int) -> int:
     if n <= 0:
         return 0
     elif n == 1:
@@ -16,6 +19,21 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2) # recursive case 
            
 # Check    
-print(fibonacci(5)) # Output -> 5
+print(fibonacci(42)) # Output -> 267914296
 print(fibonacci(12)) # Output -> 144
 print(fibonacci(-11)) # Output -> 0
+print(fibonacci(6)) # Output -> 8
+
+# Solution 2 from LinkednIn folowers
+def fibonacci2(n):
+  i = 0
+  previous, current = 0, 1
+  while i < n:
+    previous, current = current, previous + current
+    i += 1
+  return previous
+
+print(fibonacci2(42))
+print(fibonacci2(12)) # Output -> 144
+print(fibonacci2(-11)) # Output -> 0
+print(fibonacci2(6)) # Output -> 8
